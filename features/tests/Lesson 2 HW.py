bae771f70463fc625819b445ed07f7dc37301bd1
Elizabeth.T <beth10pow@gmail.com>
2024-03-09 15:07:21 -0500

    Fix bad repo

--- features/tests/Lesson 2 HW.py
+++ features/tests/Lesson 2 HW.py
@@ -12,4 +12,19 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+driver.get('https://www.amazon.com/sign-in')
+driver.find_element(By.XPATH, "//i[@aria-label='Amazon']")
+driver.find_element(By.XPATH, "//input[@id='ap_email']")
+driver.find_element(By.XPATH, "//input[@aria-labelledby='continue-announce']")
+
+#note: I do not think these searches for 'Conditions of Use' and 'Privacy Notice' are correct and I am having issues with finding links
+
+#driver.find_element(By.XPATH, "//a[@href=/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=508088'>Conditions of Use</a>']")
+#driver.find_element(By.XPATH, "//a[@href=/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&amp;nodeId=468496>Privacy Notice</a>']")
+
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+driver.find_element(By.XPATH, "//a[@id='auth-fpp-link-bottom']")
+driver.find_element(By.XPATH, "//a[@class='a-link-normal']")
+driver.find_element(By.XPATH, "//a[@id='createAccountSubmit']")
+
 sleep(4)
