34b499f220b5f6075ad95c92b1603909b3618144
Elizabeth.T <beth10pow@gmail.com>
2024-03-10 19:04:10 -0400

    updated Lesson 4 homework

--- features/steps/Target_search_steps.py
+++ features/steps/Target_search_steps.py
@@ -16,7 +16,7 @@ def click_cart(context):
 @then('verify “Your cart is empty” message is shown')
 def verify_cart_empty_message(context):
     actual_text = context.driver.find_element(By.CSS_SELECTOR,  "h1[class*='StyledHeading']").text
-    assert 'your cart is empty' == actual_text, f"expected 'your cart is empty' but got {actual_text}"
+    assert 'Your cart is empty' == actual_text, f"expected 'Your cart is empty' but got {actual_text}"
 
 
 @when('Click sign in')
@@ -33,4 +33,46 @@ def right_click_sign_in(context):
 def verify_sign_in_form(context):
     sign_in_message = context.driver.find_element(By.CSS_SELECTOR, "[data-test='content-wrapper']").text
     assert 'Sign into your Target account' == sign_in_message, f"expected 'Sign into your Target account' but got {sign_in_message}"
+
+
+
+@when('Click Target Circle')
+def click_target_circle(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/GlobalHeader/UtilityHeader/TargetCircle']").click()
+
+
+@then('Verify there are 5 Benefits to Target Circle')
+def verify_benefits_to_target(context):
+    benefits_to_target = context.driver.find_elements(By.CSS_SELECTOR, "li[class*='BenefitCard']")
+    assert len(benefits_to_target) == 5, f"expected '5' but got {len(benefits_to_target)}"
+
+
+@when ('Search for {product}')
+def search_product(context, product):
+    print(product)
+    context.driver.find_element(By.ID, 'search').send_keys(product)
+    context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
+
+
+@when ('Click on Add to cart button')
+def click_on_cart(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[data-test='chooseOptionsButton']").click()
+
+    sleep(3)
+
+
+@when ('Confirm Add to cart on side navigation')
+def click_on_side_navigation(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[id*='addToCartButton']").click()
+
+
+@when('View Cart and check out')
+def view_cart_checkout(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[class*='styles__ButtonSecondary']").click()
+
+
+@then('Verify items are in cart')
+def verify_items_in_cart(context):
+    items = context.driver.find_elements(By.CSS_SELECTOR, "[class*='styles__CartSummary']")
+    assert 'items in cart' == items[0].text, f"Expected 'items in cart' but got {items[0].text}"
 
--- features/tests/ET_Target_Search.feature
+++ features/tests/ET_Target_Search.feature
@@ -13,4 +13,15 @@ Feature: Shopping Cart tests
     When From right side navigation menu, click Sign In
     Then Verify Sign In form opened
 
+  Scenario: User can Review all 5 benefits of Target Circle
+    Given Open Target main page
+    When Click Target Circle
+    Then Verify there are 5 Benefits to Target Circle
 
+  Scenario: User can add items to cart
+    Given Open Target main page
+    When Search for Coffee
+    And Click on Add to cart button
+    And Confirm Add to cart on side navigation
+    And View cart and check out
+    Then Verify items are in cart
